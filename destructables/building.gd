150e06a699886d63ecaf54a9d86c43d711bb5af8
Wake1st <joeltwakefield@gmail.com>
2025-10-02 22:36:30 -0600

    building damage, collapse, and rumble

---
+++ destructables/building.gd
@@ -0,0 +1,52 @@
+class_name Building
+extends StaticBody3D
+
+
+signal destroyed(value: float)
+
+const FALLING_RATE: float = 0.4
+const FALLING_CUTOFF: float = -4.0
+const RUMBLE_AMPLITUDE: float = 0.0024
+
+@export var health: float = 100
+@export var cost: float = 400
+
+@export var rumble_dampen: float = 0.01
+
+var rumble_magnitude: float
+var is_collapsing: bool
+
+
+func damage(value: float) -> void:
+	health -= value
+	
+	# animate rumble
+	rumble_magnitude = value * RUMBLE_AMPLITUDE
+	
+	# building is dead
+	if health <= 0:
+		destroyed.emit(cost)
+		is_collapsing = true
+
+
+func _process(delta) -> void:
+	# shake n bake
+	if rumble_magnitude > 0:
+		# set random position in XZ plane
+		position.x = randf_range(-rumble_magnitude, rumble_magnitude)
+		position.z = randf_range(-rumble_magnitude, rumble_magnitude)
+		
+		# reduce magnitude
+		rumble_magnitude -= rumble_dampen * delta
+	
+	# sink into the ground
+	if is_collapsing:
+		# slide into the ground
+		move_and_collide(Vector3(0, -FALLING_RATE * delta, 0))
+		
+		# keep rumblin
+		rumble_magnitude += rumble_dampen * delta
+		
+		# don't keep around if destroyed
+		if position.y < FALLING_CUTOFF:
+			queue_free()
